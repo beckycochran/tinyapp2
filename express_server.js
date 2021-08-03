06a1821f2dda12c9cc3ba8a2f8e39fa1748c1a1a
Rebekah <rebekahmparkes@outlook.com>
2021-08-03 15:43:52 -0400

    delete button

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.set('view engine', 'ejs');
 
@@ -8,6 +11,11 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  return (Math.random().toString(36).slice(2)).slice(0,(5));
+}
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -25,19 +33,56 @@ app.get("/urls.json", (req, res) => {
 // JSON string representing entire urlDatabase object
 
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  console.log(req.body.longURL);
+  urlDatabase[shortURL] = longURL;
+
+  const templateVars = { urls: urlDatabase };
+
+  res.render("urls_index", templateVars);
+  
+  res.redirect(`/urls/:${shortURL}`);
+
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
+
   const templateVars = { shortURL, longURL };
+  
   res.render("urls_show", templateVars);
+});
+
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+
+  res.redirect(longURL);
+});
+
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+
+  delete urlDatabase[shortURL];
+  console.log(urlDatabase, 'urlDatabase');
+  console.log(shortURL, 'shortURL');
+
+
+  res.redirect(`/urls`);
+
 });
