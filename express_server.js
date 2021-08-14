246888218ec3c16363107d649b8665e99237ab01
Rebekah <rebekahmparkes@outlook.com>
2021-08-13 22:11:14 -0400

    final stuff

--- express_server.js
+++ express_server.js
@@ -4,11 +4,11 @@ const { getUserByEmail, generateRandomString } = require("./helpers");
 
 const cookieSession = require('cookie-session');
 const app = express();
-const PORT = 8080; 
+const PORT = 8080;
 
 app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieSession({
   name: 'session',
@@ -17,24 +17,24 @@ app.use(cookieSession({
 
 const urlDatabase = {
   b6UTxQ: {
-      longURL: "https://www.tsn.ca",
-      userID: "aJ48lW"
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW"
   },
   i3BoGr: {
-      longURL: "https://www.google.ca",
-      userID: "aJ48lW"
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW"
   }
 };
 
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "1@1.com", 
+    id: "userRandomID",
+    email: "1@1.com",
     password: "1"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 }
@@ -47,8 +47,13 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+
+/// ----------------------------------------------  ///
+
+/// ------------------  / ------------------  ///
+
 app.get("/", (req, res) => {
-  const id = req.session.userID;  
+  const id = req.session.userID;
   if (id) {
     res.redirect('/urls');
   }
@@ -64,11 +69,11 @@ app.get("/urls", (req, res) => {
   const user = users[id];
   const shortURLS = urlDatabase;
 
-   const templateVars = { 
-     shortURLS,
-     user
-    };
-   res.render("urls_index", templateVars);
+  const templateVars = {
+    shortURLS,
+    user
+  };
+  res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -76,16 +81,17 @@ app.post("/urls", (req, res) => {
   const longURL = req.body.longURL;
   console.log(req.body.longURL);
 
-  const userID = req.session.userID;
-  const user = users[userID];
+  const id = req.session.userID;
+  const user = users[id];
 
-  urlDatabase[shortURL] = {longURL, userID};
+  urlDatabase[shortURL] = { longURL, userID: id };
 
   const templateVars = { shortURLS: urlDatabase, user };
   res.render("urls_index", templateVars);
 });
 
 
+/// ----------------------------------------------  ///
 
 /// ------------------  /urls/new ------------------  ///
 
@@ -95,11 +101,29 @@ app.get("/urls/new", (req, res) => {
   const id = req.session.userID;
   const user = users[id];
 
-  const templateVars = { shortURL, longURL, user};
+  const templateVars = { shortURL, longURL, user };
 
   res.render("urls_new", templateVars);
 });
 
+
+/// ----------------------------------------------  ///
+
+/// ------------------  /urls/:shortURL ------------------  ///
+
+app.get("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL].longURL;
+  const id = req.session.userID;
+  const user = users[id];
+  
+  const templateVars = { 
+    shortURL, 
+    longURL,
+    user
+  };
+  res.render("urls_show", templateVars);
+});
 
 app.post("/urls/:shortURL", (req, res) => {
   const id = req.session.userID;
@@ -108,7 +132,7 @@ app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
 
-  if (urlDatabase[shortURL].userID === user.id) {  
+  if (urlDatabase[shortURL].userID === user.id) {
     urlDatabase[shortURL].longURL = longURL;
 
     res.redirect("/urls");
@@ -118,22 +142,19 @@ app.post("/urls/:shortURL", (req, res) => {
 
 })
 
+/// ----------------------------------------------  ///
+/// --------  /urls/edit  and /urls/:shortURL/delete ---  ///
+
 app.post("/urls/:shortURL/edit", (req, res) => {
   const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
+  const longURL = urlDatabase[shortURL].longURL;
+  const id = req.session.userID;
+  const user = users[id];
 
-  const templateVars = { shortURL, longURL, username };
 
+  const templateVars = { shortURL, longURL, user };
   res.render("urls_show", templateVars);
 })
-
-
-app.get("/u/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL].longURL;
-  res.redirect(longURL);
-});
-
 
 
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -148,8 +169,20 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 
-/// ----------------------------------------------  ///
 
+/// ----------------------------------------------  ///
+/// ------------------  /u/ ------------------  ///
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL].longURL;
+  res.redirect(longURL);
+
+    return res.status(400).send("Url does not exist, sorry! Please <a href= '/urls'>try again</a>");
+});
+
+
+/// ----------------------------------------------  ///
 /// ------------------  REGISTER  ------------------  ///
 
 app.get("/register", (req, res) => {
@@ -157,7 +190,7 @@ app.get("/register", (req, res) => {
   const id = req.session.userID;
   const user = users[id];
   res.render("user-reg", { user });
-  
+
 })
 
 app.post("/register", (req, res) => {
@@ -205,13 +238,13 @@ app.post("/login", (req, res) => {
   const user = getUserByEmail(email, users);
   // ---------- ERROR! User does not exist. -------------//
   if (!user) {
-    return res.status(403).send("User does not exist, or credentials are invalid. Please <a href= '/login'>try again</a>"); 
+    return res.status(403).send("User does not exist, or credentials are invalid. Please <a href= '/login'>try again</a>");
   }
   // ---------- The password corresponds to the stored hashed password -------------//
   if (bcrypt.compareSync(password, user.password)) {
     req.session.userID = user.id;
     return res.redirect(`/urls`);
-  } 
+  }
 });
 
 /// ----------------------------------------------  ///
